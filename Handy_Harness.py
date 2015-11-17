7da931eaca8317fe81bca5a5e2f1ee5dc052eebe
mrjax <quesomonos@gmail.com>
2015-11-17 11:22:08 -0600

    Finished bookmark moves
    Now can move line to previous bookmark and move line to next bookmark.
    Also, since shortcut for multiselect is ctrl+alt+up/down (very useful,
    didn't know that one), I changed extra line move shortcuts here to
    ctrl+shift+alt+up/down/left/right.

--- Handy_Harness.py
+++ Handy_Harness.py
@@ -14,6 +14,9 @@ class HandyHarnessCommand(sublime_plugin.TextCommand):
 		if args['op'] == 'move_line_to_next':
 			self.moveLineTo(edit, "next")
 
+		if args['op'] == 'move_line_to_prev':
+			self.moveLineTo(edit, "prev")
+
 
 	def moveLineTo(self, edit, dest):
 		s = self.view.sel()[0]
@@ -28,7 +31,8 @@ class HandyHarnessCommand(sublime_plugin.TextCommand):
 		while(end < size and self.view.substr(end) != '\n'):
 			end += 1
 
-		line = self.view.substr(sublime.Region(start,end)).strip()
+		#want region from start to end, plus any extra new lines (full_line).  That's still a region, so I substring it and strip leading or trailing newlines, because depending on where you put it I manually add newlines back in
+		line = self.view.substr(self.view.full_line(sublime.Region(start,end))).strip()
  
 		if dest == "bottom": 
 			self.view.sel().clear()
@@ -45,20 +49,34 @@ class HandyHarnessCommand(sublime_plugin.TextCommand):
 			self.view.insert(edit, 0, line + "\n")			
 
 		if dest == "next":
-			print "next"
 			bookmarks = self.view.get_regions('bookmarks')
 			if len(bookmarks) != 0:
-				#self.view.erase(edit, self.view.full_line(sublime.Region(start,end)))
 				index = self.view.sel()[0].a - 1
-				while index < self.view.sel()[0].a:
+				while index < self.view.sel()[0].a and len(bookmarks) >= 1:
 					index = bookmarks[0].a
 					bookmarks.pop(0)
-				
-				self.view.insert(edit, index, "\n" + line)	
+
+				self.view.insert(edit, index, "\n" +  line)
 
 				self.view.sel().clear()
 				self.view.sel().add(sublime.Region(end + 1 + s.a - start))
 				self.view.erase(edit, self.view.full_line(sublime.Region(start,end)))
+
+		if dest == "prev":
+			bookmarks = self.view.get_regions('bookmarks')
+			if len(bookmarks) != 0:
+				index = self.view.sel()[0].a + 1
+				while index > self.view.sel()[0].a and len(bookmarks) >= 1:
+					index = bookmarks[len(bookmarks) - 1].a
+					bookmarks.pop(len(bookmarks) - 1)
+
+				#must remove line before inserting because insertion will change deletion points
+				self.view.sel().clear()
+				self.view.sel().add(sublime.Region(end + 1 + s.a - start))
+				self.view.erase(edit, self.view.full_line(sublime.Region(start,end)))
+
+				self.view.insert(edit, index, "\n" +  line)
+
 						
 
 
